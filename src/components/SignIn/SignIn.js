a636c78708e1cefe2590e9e25e76ccc5ac4833e9
farias-77 <gabrielfalves2003@gmail.com>
2022-09-27 14:51:00 -0300

    feat: sign-in page (complete)

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,35 +1,42 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Bars } from "react-loader-spinner";
 import logo from "../../assets/logo.svg";
 import styled from "styled-components";
-import { useState } from "react";
 import axios from "axios";
 
 export default function SignIn() {
     const navigate = useNavigate();
 
-    const [signInData, setSignInData] = useState({
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            navigate("/home");
+        }
+    }, []);
+
+    const [displayLoading, setDisplayLoading] = useState(false);
+    const [displayInvalid, setDisplayInvalid] = useState(false);
+    const [credentials, setCredentials] = useState({
         email: "",
         password: "",
     });
 
-    function submitSignIn(e) {
-        e.preventDefault();
-        console.log("chegou");
-
+    function submitSignIn() {
         const url = "https://superzaprecall.onrender.com/sign-in";
-        const promise = axios.post(url, signInData);
+        const promise = axios.post(url, credentials);
+        setDisplayLoading(true);
 
         promise
             .then((res) => {
-                console.log(res.data);
+                const token = res.data.token;
+                localStorage.setItem("token", token);
+                navigate("/home");
             })
             .catch((err) => {
-                console.log(err);
+                setCredentials({ email: "", password: "" });
+                setDisplayInvalid(true);
+                setDisplayLoading(false);
             });
-    }
-
-    function navigateToSignUp() {
-        navigate("/signUp");
     }
 
     return (
@@ -37,14 +44,59 @@ export default function SignIn() {
             <Logo>
                 <img src={logo} alt="logo" />
                 <h1>PantherCards</h1>
+                <WhiteBackGround />
             </Logo>
+
             <Form>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Senha" />
-                <Button onClick={submitSignIn}>Enviar</Button>
+                {displayInvalid ? (
+                    <InvalidCredentials>
+                        Email ou senha incorretos.
+                    </InvalidCredentials>
+                ) : (
+                    <></>
+                )}
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={credentials.email}
+                    onChange={(e) =>
+                        setCredentials({
+                            ...credentials,
+                            email: e.target.value,
+                        })
+                    }
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={credentials.password}
+                    onChange={(e) =>
+                        setCredentials({
+                            ...credentials,
+                            password: e.target.value,
+                        })
+                    }
+                />
+
+                <Button onClick={submitSignIn}>
+                    {displayLoading ? (
+                        <Bars
+                            height="40"
+                            color="white"
+                            ariaLabel="Loading..."
+                        />
+                    ) : (
+                        <h5>Enviar</h5>
+                    )}
+                </Button>
             </Form>
 
-            <SignUpLink onClick={navigateToSignUp}>
+            <SignUpLink
+                onClick={() => {
+                    navigate("/signUp");
+                }}
+            >
                 <u>
                     Ainda não tem uma conta?
                     <br />
@@ -73,15 +125,18 @@ const Container = styled.div`
 `;
 
 const Logo = styled.div`
-    width: 20%;
+    width: 50%;
 
     display: flex;
     flex-direction: column;
     align-items: center;
 
+    position: relative;
+
     img {
-        width: 300px;
-        background-color: white;
+        z-index: 1;
+        width: 30%;
+        object-fit: cover;
     }
 
     h1 {
@@ -90,15 +145,30 @@ const Logo = styled.div`
         color: white;
         text-align: center;
     }
+
+    @media (max-width: 950px) {
+        width: 100%;
+    }
+`;
+
+const WhiteBackGround = styled.div`
+    width: 20%;
+    height: 73%;
+
+    position: absolute;
+    top: 5px;
+
+    background-color: white;
 `;
 
 const Form = styled.div`
     margin-top: 50px;
 
-    width: 25%;
+    width: 35%;
 
     display: flex;
     flex-direction: column;
+    align-items: center;
 
     input {
         width: 100%;
@@ -121,6 +191,14 @@ const Form = styled.div`
             color: #9c9c9c;
         }
     }
+
+    @media (max-width: 950px) {
+        width: 100%;
+
+        input {
+            width: 80%;
+        }
+    }
 `;
 
 const Button = styled.div`
@@ -135,15 +213,30 @@ const Button = styled.div`
     align-items: center;
     justify-content: center;
 
-    font-family: "Krona One", sans-serif;
-    font-size: 15px;
-    color: white;
+    h5 {
+        font-family: "Krona One", sans-serif;
+        font-size: 15px;
+        color: white;
+    }
 
     cursor: pointer;
+
+    @media (max-width: 950px) {
+        width: 80%;
+    }
+`;
+
+const InvalidCredentials = styled.div`
+    font-family: "Krona One", sans-serif;
+    font-size: 17px;
+    color: white;
+    text-align: center;
+
+    margin-bottom: 20px;
 `;
 
 const SignUpLink = styled.div`
-    width: 20%;
+    width: 90%;
 
     margin-top: 20px;
 
