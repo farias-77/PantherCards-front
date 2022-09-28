f6186558a0117e2e602dea4fdede362924ed02f2
farias-77 <gabrielfalves2003@gmail.com>
2022-09-28 12:24:36 -0300

    fix: home page covers all DOM

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,27 @@
 import styled from "styled-components";
 
 export default function Home() {
-    return <></>;
+    return (
+        <Container>
+            <HomePage></HomePage>
+        </Container>
+    );
 }
 
 const Container = styled.div`
-    padding-top: 100px;
-
-    height: 100vh;
     width: 100vw;
+    height: 100vh;
 
-    background-color: #333333;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: #323232;
+`;
+
+const HomePage = styled.div`
+    width: 60%;
+    height: 100px;
+
+    background-color: yellow;
 `;
